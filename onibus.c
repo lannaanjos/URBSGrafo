6363d5be109f24bc63012b4024cc2a0cab61f6fd
Lanna <lannadosanjoscosta@gmail.com>
2025-06-16 12:07:00 -0300

    update: csv + codigo

--- onibus.c
+++ onibus.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
+//  Estrutura dos Pontos de Ônibus
+typedef struct Ponto{
+    int destino;
+    struct Ponto *proximo;
+}Ponto;
+
+#define QntdPontos 17100
+#define QntdFila 17100
+
+//  Declarado globalmente e como null para não ter lixo de memória e ficar acessível em todo o programa
+Ponto* paradas[QntdPontos] = {NULL};
+
+//  Protótipos
+void criarponto(int origem, int destino);
+
 int main(){
     return 0;
+}
+
+//  Funções e Procedimentos
+void criarponto(int origem, int destino){
+    Ponto* novoPonto = malloc(sizeof(Ponto));
+
+    if(novoPonto == NULL){
+        printf("Alocação mal sucedida\nEncerrando...");
+        exit(0);
+    }
+
+    novoPonto->destino = destino;
+    novoPonto->proximo = paradas[origem];
+    paradas[origem] = novoPonto;
 }
